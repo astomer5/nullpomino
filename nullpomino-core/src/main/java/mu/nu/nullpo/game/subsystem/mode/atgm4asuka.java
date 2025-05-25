3fa7de5b0560ccc6d60d5dde367c49f37e10e238
astomer5 <2976466417@qq.com>
2025-05-25 18:13:40 +0800

    Updated some files

---
+++ nullpomino-core/src/main/java/mu/nu/nullpo/game/subsystem/mode/atgm4asuka.java
@@ -0,0 +1,19 @@
+package mu.nu.nullpo.game.subsystem.mode;
+
+import java.util.Random;
+
+import mu.nu.nullpo.game.component.BGMStatus;
+import mu.nu.nullpo.game.component.Controller;
+import mu.nu.nullpo.game.event.EventReceiver;
+import mu.nu.nullpo.game.play.GameEngine;
+import mu.nu.nullpo.util.CustomProperties;
+import mu.nu.nullpo.util.GeneralUtil;
+
+import org.apache.log4j.Logger;
+
+public class atgm4asuka extends AbstractMode {
+    private static final int[] tableGravityValue =
+            {
+                    -1
+            };
+}
